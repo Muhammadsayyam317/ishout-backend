36759b686b3eb39886687ad9e896899bd37bcf22
Muhammadsayyam317 <sayyam.techtimize@gmail.com>
2025-12-05 12:23:55 +0500

    add annotated sender id

--- app/models/whatsappconversation_model.py
+++ app/models/whatsappconversation_model.py
@@ -1,8 +1,17 @@
-from typing import TypedDict, Optional
+from typing import TypedDict, Optional, Annotated
+
+
+def take_first_sender_id(a, b):
+    """Binary reducer that takes the first non-None value for sender_id.
+    This prevents InvalidUpdateError when multiple nodes return state with sender_id.
+    LangGraph expects a binary function (a, b) -> c."""
+    if a is not None:
+        return a
+    return b
 
 
 class ConversationState(TypedDict, total=False):
-    sender_id: Optional[str]
+    sender_id: Annotated[Optional[str], take_first_sender_id]
     user_message: Optional[str]
     intent: Optional[str]
 
@@ -14,3 +23,7 @@ class ConversationState(TypedDict, total=False):
     reply: Optional[str]
     last_active: Optional[float] = None
     event_data: dict
+    campaign_id: Optional[str]
+    done: Optional[bool]
+    reply_sent: Optional[bool]
+    thread_id: Optional[str]
