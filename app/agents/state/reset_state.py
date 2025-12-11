049270282f5dd8e2a54c1b1047c52415055c3088
Muhammadsayyam317 <sayyam.techtimize@gmail.com>
2025-12-11 15:37:38 +0500

    add user states

---
+++ app/agents/state/reset_state.py
@@ -0,0 +1,5 @@
+from app.agents.state.create_user_state import create_new_state
+
+
+async def reset_user_state(sender_id):
+    return await create_new_state(sender_id)
---
+++ app/agents/state/update_user_state.py
@@ -0,0 +1,14 @@
+import time
+from app.db.mongo_session import get_session_collection
+
+
+async def update_user_state(sender_id, new_data: dict):
+    session_collection = get_session_collection()
+    new_data.pop("_id", None)
+    new_data["last_active"] = time.time()
+    await session_collection.update_one(
+        {"sender_id": sender_id}, {"$set": new_data}, upsert=True
+    )
+    updated = await session_collection.find_one({"sender_id": sender_id})
+    updated.pop("_id", None)
+    return updated
