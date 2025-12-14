f0c8e8f8198152c93836651ad5f8b9de834e7ebd
Muhammadsayyam317 <sayyam.techtimize@gmail.com>
2025-12-14 23:28:51 +0500

    redis configuration

--- app/core/redis.py
+++ app/core/redis.py
@@ -2,11 +2,12 @@ from langgraph.checkpoint.redis import RedisSaver
 from app.agents.graph.whatsapp_graph import graph
 from app.config.credentials_config import config
 
-redis_url = f"redis://{config.REDIS_USERNAME}:{config.REDIS_PASSWORD}@{config.REDIS_HOST}:{config.REDIS_PORT}"
+
+REDIS_URI = f"redis://{config.REDIS_USERNAME}:{config.REDIS_PASSWORD}@{config.REDIS_HOST}:{config.REDIS_PORT}"
 
 
 async def redis_checkpointer():
-    with RedisSaver.from_conn_string(redis_url) as checkpointer:
+    with RedisSaver.from_conn_string(REDIS_URI) as checkpointer:
         checkpointer.setup()
     compiled_graph = graph.compile(checkpointer=checkpointer)
     return compiled_graph
