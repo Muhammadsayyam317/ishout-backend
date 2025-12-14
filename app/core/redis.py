fb9685c88a6dd49c44cf48233e856bb584533f71
Muhammadsayyam317 <sayyam.techtimize@gmail.com>
2025-12-14 23:06:04 +0500

    add checkpointer

--- app/core/redis.py
+++ app/core/redis.py
@@ -4,12 +4,9 @@ from app.config.credentials_config import config
 
 redis_url = f"redis://{config.REDIS_USERNAME}:{config.REDIS_PASSWORD}@{config.REDIS_HOST}:{config.REDIS_PORT}"
 
-checkpointer = RedisSaver.from_conn_string(redis_url)
-checkpointer.ttl = 60 * 60 * 24
-checkpointer.key_prefix = "whatsapp:agent"
-
 
 async def redis_checkpointer():
-    await checkpointer.setup()
+    with RedisSaver.from_conn_string(redis_url) as checkpointer:
+        checkpointer.setup()
     compiled_graph = graph.compile(checkpointer=checkpointer)
     return compiled_graph
