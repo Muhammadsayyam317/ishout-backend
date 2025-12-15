caff29ae830bcf489bef81b09064fe1bb8d2ae01
Muhammadsayyam317 <sayyam.techtimize@gmail.com>
2025-12-15 12:34:13 +0500

    async context manager

--- app/core/redis.py
+++ app/core/redis.py
@@ -2,19 +2,24 @@ from langgraph.checkpoint.redis.aio import AsyncRedisSaver
 from app.agents.graph.whatsapp_graph import graph
 from app.config.credentials_config import config
 
-REDIS_URI = f"redis://{config.REDIS_USERNAME}:{config.REDIS_PASSWORD}@{config.REDIS_HOST}:{config.REDIS_PORT}"
+REDIS_URI = f"redis://:{config.REDIS_PASSWORD}@{config.REDIS_HOST}:{config.REDIS_PORT}"
 
 whatsapp_agent = None
+checkpointer_contextmanager = None
 
 
 async def init_redis_agent():
-    global whatsapp_agent, checkpointer
+    global whatsapp_agent, _checkpointer_cm
     if whatsapp_agent is not None:
         return
-    async with AsyncRedisSaver.from_conn_string(
+
+    # Create context manager
+    checkpointer = AsyncRedisSaver.from_conn_string(
         REDIS_URI,
         ttl=60 * 60 * 24,
-    ) as checkpointer:
-        await checkpointer.setup()
-        whatsapp_agent = graph.compile(checkpointer=checkpointer)
-        print("WhatsApp agent initialized with Redis")
+    )
+
+    # enter into async context manager
+    checkpointer = await checkpointer.__aenter__()
+    whatsapp_agent = graph.compile(checkpointer=checkpointer)
+    print("WhatsApp agent initialized with Redis")
