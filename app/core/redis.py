6b6684209ba9c59689fec510ec6c7bfae1af51bf
Muhammadsayyam317 <sayyam.techtimize@gmail.com>
2025-12-15 11:50:17 +0500

    verify token in middleware

--- app/core/redis.py
+++ app/core/redis.py
@@ -5,13 +5,16 @@ from app.config.credentials_config import config
 REDIS_URI = f"redis://{config.REDIS_USERNAME}:{config.REDIS_PASSWORD}@{config.REDIS_HOST}:{config.REDIS_PORT}"
 
 whatsapp_agent = None
-checkpointer = None
 
 
 async def init_redis_agent():
     global whatsapp_agent, checkpointer
     if whatsapp_agent is not None:
         return
-    async with AsyncRedisSaver.from_conn_string(REDIS_URI) as checkpointer:
+    async with AsyncRedisSaver.from_conn_string(
+        REDIS_URI,
+        ttl=60 * 60 * 24,
+    ) as checkpointer:
         await checkpointer.setup()
         whatsapp_agent = graph.compile(checkpointer=checkpointer)
+        print("WhatsApp agent initialized with Redis")
