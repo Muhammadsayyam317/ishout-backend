cbf6b2f5d1297d326d67e0a2e2db65f9344144e1
Muhammadsayyam317 <sayyam.techtimize@gmail.com>
2025-12-19 15:01:10 +0500

    change user profile routes

--- app/api/routes/company_routes.py
+++ app/api/routes/company_routes.py
@@ -20,6 +20,8 @@ from app.models.campaign_model import (
     UserRejectInfluencersRequest,
 )
 from app.tools.search_influencers import search_influencers
+from app.api.controllers.company.profile import get_user_profile, update_user_profile
+from app.api.controllers.auth_controller import change_password
 
 router = APIRouter()
 
@@ -90,4 +92,27 @@ router.add_api_route(
     endpoint=companyApprovedSingleInfluencer,
     methods=["PATCH"],
     tags=["Company"],
+)
+
+
+# PROFILE ROUTES
+router.add_api_route(
+    path="/update-profile/{user_id}",
+    endpoint=update_user_profile,
+    methods=["PUT"],
+    tags=["User"],
+)
+
+router.add_api_route(
+    path="/user-profile/{user_id}",
+    endpoint=get_user_profile,
+    methods=["GET"],
+    tags=["User"],
+)
+
+router.add_api_route(
+    path="/change-password",
+    endpoint=change_password,
+    methods=["PUT"],
+    tags=["User"],
 )
