185a407ca7196468f8a59f09b0b7dc8c94b4c096
Muhammadsayyam317 <sayyam.techtimize@gmail.com>
2026-01-01 15:27:55 +0500

    add mongo session

--- app/db/mongo_session.py
+++ app/db/mongo_session.py
@@ -1,4 +1,4 @@
-from app import config
+from app.config.credentials_config import config
 from app.db.connection import get_db
 from pymongo import ASCENDING
 
@@ -6,10 +6,14 @@ SESSION_EXPIRY_SECONDS = 600  # 10 minutes
 
 
 def get_session_collection():
+    print("Getting session collection")
+    print(f"Collection name: {config.MONGODB_ATLAS_COLLECTION_WHATSAPP_SESSIONS}")
     db = get_db()
+    print(f"DB: {db}")
     session_collection = db.get_collection(
         config.MONGODB_ATLAS_COLLECTION_WHATSAPP_SESSIONS
     )
+    print(f"Session collection: {session_collection}")
     session_collection.create_index(
         [("last_active", ASCENDING)],
         expireAfterSeconds=SESSION_EXPIRY_SECONDS,
