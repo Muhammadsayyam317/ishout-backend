d5acd6903a857d8bfbbb39afd20ab955d1930356
Muhammadsayyam317 <sayyam.techtimize@gmail.com>
2026-01-15 15:26:21 +0500

    add logs

--- app/services/instagram/send_instagram_message.py
+++ app/services/instagram/send_instagram_message.py
@@ -12,22 +12,25 @@ async def Send_Insta_Message(message: str, recipient_id: str):
         backend_url = (
             f"https://graph.facebook.com/{config.IG_GRAPH_API_VERSION}/me/messages"
         )
-        print(f"backend_url: {backend_url}")
+
         headers = {
             "Authorization": f"Bearer {PAGE_ACCESS_TOKEN}",
             "Content-Type": "application/json",
         }
         payload = {"recipient": {"id": recipient_id}, "message": {"text": message}}
-        print(f"payload: {payload}")
+        print(f"Sending message to {recipient_id}: {message}")
+        print(f"POST URL: {backend_url}")
+        print(f"Payload: {payload}")
+
         async with httpx.AsyncClient(timeout=10.0) as client:
             response = await client.post(backend_url, json=payload, headers=headers)
-            print(f"response: {response.text}")
+            print(f"IG DM response: {response.text}")
             if response.status_code != 200:
                 raise InternalServerErrorException(
                     message=f"Error sending message: {response.text}"
                 )
-            print(f"Message sent to {recipient_id}: {message}")
-            print("Message sent successfully")
+            print(f"Message sent Successfully to {recipient_id}: {message}")
+            print("Exiting from Send_Insta_Message")
             return True
     except Exception as e:
         print(f"Error sending message: {e}")
