4e5572998a75e49804e42ef1a849391d52a16837
Muhammadsayyam317 <sayyam.techtimize@gmail.com>
2026-01-15 18:04:35 +0500

    feat:add logs for debug whatsapp agent

--- app/agents/Whatsapp/nodes/notify_admin.py
+++ app/agents/Whatsapp/nodes/notify_admin.py
@@ -5,6 +5,7 @@ from app.services.whatsapp.onboarding_message import send_whatsapp_message
 async def node_notify_admin_campaign_created(campaign, user):
     print("Entering into node_notify_admin_campaign_created")
     try:
+        print(f"Campaign: {campaign} User: {user}")
         message = (
             "🚨 *New Campaign Created*\n\n"
             f"🏢 Company: {user.get('company_name')}\n"
@@ -17,9 +18,11 @@ async def node_notify_admin_campaign_created(campaign, user):
         )
         print("Message: ", message)
         success = await send_whatsapp_message(config.ADMIN_PHONE, message)
+        print(f"Success: {success}")
         if not success:
             raise Exception("Failed to send message to admin")
         print("Exiting from notify_admin_campaign_created")
+        print(f"Success: {success}")
         return True
     except Exception as e:
         print("❌ Error in notify_admin_campaign_created:", e)
