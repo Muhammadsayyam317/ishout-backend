5650030fd5641a1377e9dc802160ea958647dc29
Muhammadsayyam317 <sayyam.techtimize@gmail.com>
2026-02-14 14:31:36 +0500

    add logs

--- app/services/whatsapp/save_negotiation_message.py
+++ app/services/whatsapp/save_negotiation_message.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timezone
 from app.core.exception import InternalServerErrorException
 from app.db.connection import get_db
+from app.utils.printcolors import Colors
 
 
 async def save_negotiation_message(
@@ -12,7 +13,8 @@ async def save_negotiation_message(
     human_takeover: bool = False,
     conversation_mode: str = "NEGOTIATION",
 ):
-    print("Entering into save_conversation_message")
+    print(f"{Colors.GREEN}Entering into save Negotiation message")
+    print("--------------------------------")
     try:
         payload = {
             "thread_id": thread_id,
@@ -28,17 +30,17 @@ async def save_negotiation_message(
         db = get_db()
         collection = db.get_collection("whatsapp_negotiation")
         await collection.insert_one(payload)
-        print("Payload: ", payload)
+
         print("--------------------------------")
         print("Whatsapp Conversation message saved")
         print("--------------------------------")
+        print(f"{Colors.CYAN} Exiting from save Negotiation message")
         return payload
 
     except Exception as e:
-        print("Error in saving conversation message")
-        print("--------------------------------")
-        print(e)
+        print(f"{Colors.RED}Error in saving conversation message: {e}")
         print("--------------------------------")
         raise InternalServerErrorException(
             message=f"Error in saving conversation message: {str(e)}"
         ) from e
+        print("--------------------------------")
