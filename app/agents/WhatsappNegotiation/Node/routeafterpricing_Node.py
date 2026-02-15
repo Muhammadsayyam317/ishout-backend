04690833c5d9047202a16043c05bf194d70f7f7b
Muhammadsayyam317 <sayyam.techtimize@gmail.com>
2026-02-15 14:21:50 +0500

    add router after price fetching

--- app/agents/WhatsappNegotiation/Node/routeafterpricing_Node.py
+++ app/agents/WhatsappNegotiation/Node/routeafterpricing_Node.py
@@ -5,24 +5,24 @@ from app.Schemas.whatsapp.negotiation_schema import (
 from app.utils.printcolors import Colors
 
 
-def route_by_intent(state: WhatsappNegotiationState):
-    print("Routing after pricing...")
+def route_after_pricing(state: WhatsappNegotiationState):
     print(f"{Colors.GREEN}Entering into route_after_pricing")
     print("--------------------------------")
     print(f"{Colors.CYAN}Routing after pricing: {state['intent']}")
     print("--------------------------------")
 
-    if state["intent"] == WhatsappMessageIntent.REJECT:
+    if state["intent"] == WhatsappMessageIntent.NEGOTIATE:
+        return "counter_offer"
+
+    if state["intent"] == WhatsappMessageIntent.ACCEPT:
         return "generate_reply"
 
-    if state["intent"] in (
-        WhatsappMessageIntent.INTEREST,
-        WhatsappMessageIntent.NEGOTIATE,
-        WhatsappMessageIntent.QUESTION,
-        WhatsappMessageIntent.ACCEPT,
-    ):
-        return "fetch_pricing"
+    if state["intent"] == WhatsappMessageIntent.QUESTION:
+        return "generate_reply"
 
+    if state["intent"] == WhatsappMessageIntent.INTEREST:
+        return "generate_reply"
     print(f"{Colors.CYAN}Exiting from route_after_pricing")
     print("--------------------------------")
+
     return "generate_reply"
